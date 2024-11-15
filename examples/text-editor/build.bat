985c25bb5a5176235d07bd11c2e7d4cc001ba5fc
박성원 <skfhddlg@naver.com>
2024-11-15 17:04:26 +0900

    hide console

--- examples/text-editor/build.bat
+++ examples/text-editor/build.bat
@@ -1,3 +1,3 @@
 rem choco install golang mingw winrar -y
-go build -ldflags="-w -extldflags=-static" ./main.go
+go build -ldflags="-w -extldflags=-static -H windowsgui" ./main.go
 "C:\Program Files\WinRAR\rar" a -r -sfx -z"config.txt" text-editor.exe main.exe ui\
